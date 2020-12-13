33ca45113fff8a178e3e031bc0a5b7dc035403e3
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-12-13 07:38:06 -0800

    Temporary Trials of hybrid and improved querries.

--- application/helper_functions.py
+++ application/helper_functions.py
@@ -2,7 +2,7 @@ from flask import current_app as app
 from functools import wraps
 from flask_login import current_user
 from sqlalchemy import or_
-from .model_db import User, Insight, Audience, Post, Campaign, db
+from .model_db import User, Insight, Audience, Post, Campaign, user_campaign, brand_campaign, db
 from datetime import timedelta, datetime as dt
 from time import time
 import json
@@ -79,6 +79,11 @@ def get_daily_ig_accounts(active=True, camp=True, camp_only=False):
         return None
     users = User.query.filter(User.instagram_id.isnot(None))
     if active:
+        if camp == 'joins':
+            active_i = User.query.join(user_campaign).join(Campaign).filter(Campaign.completed is False)
+            active_b = User.query.join(brand_campaign).join(Campaign).filter(Campaign.completed is False)
+            users = active_i.union(active_b)
+            users = users.all()
         if camp:
             is_active = Campaign.completed.is_(False)
             users = users.filter(or_(User.campaigns.any(is_active), User.brand_campaigns.any(is_active)))
@@ -88,6 +93,112 @@ def get_daily_ig_accounts(active=True, camp=True, camp_only=False):
     else:
         users = users.all()
     return users
+
+
+def get_test_ig(version):
+    """Trying a few structures to test their speed. """
+    # results = {'version': ('query', 'query_result', 'duration')}
+    if version == 'a':
+        start = time()
+        users = User.query.filter(User.has_active_special == True)
+        result = users.all()
+        end = time()
+        results = (users, result, len(result), end - start, )
+    elif version == 'b':
+        start = time()
+        users = User.query.filter(User.has_active_special is True)
+        result = users.all()
+        end = time()
+        results = (users, result, len(result), end - start, )
+    elif version == 'c':
+        start = time()
+        users = User.query.filter(User.has_active_special.is_(True))
+        result = users.all()
+        end = time()
+        results = (users, result, len(result), end - start, )
+    elif version == 'd':
+        start = time()
+        users = User.query.filter(User.has_active_connect.is_(True))
+        result = users.all()
+        end = time()
+        results = (users, result, len(result), end - start, )
+    elif version == 'e':
+        start = time()
+        users = User.query.filter(User.has_active_now.is_(True))
+        result = users.all()
+        end = time()
+        results = (users, result, len(result), end - start, )
+
+    # start_a = time()
+    # active_i = User.query.join(user_campaign).join(Campaign).filter(Campaign.completed is False)
+    # active_b = User.query.join(brand_campaign).join(Campaign).filter(Campaign.completed is False)
+    # active_lla = active_i.union(active_b)
+    # result_a = active_lla.all()
+    # end_a = time()
+    # results['a'] = (active_lla, result_a, end_a - start_a, )
+    # db.session.flush()
+
+    # start_b = time()
+    # activei = User.query.join(user_campaign).join(Campaign)
+    # activeb = User.query.join(brand_campaign).join(Campaign)
+    # active_llb = activei.union(activeb).filter(Campaign.completed is False)
+    # result_b = active_llb.all()
+    # end_b = time()
+    # results['b'] = (active_llb, result_b, end_b - start_b, )
+    # db.session.flush()
+
+    # start_c = time()
+    # i_active = Campaign.query.join(user_campaign).join(User).filter(Campaign.completed is False)
+    # b_active = Campaign.query.join(brand_campaign).join(User).filter(Campaign.completed is False)
+    # active_llc = i_active.union(b_active)
+    # result_c = active_llc.all()
+    # end_c = time()
+    # results['c'] = (active_llc, result_c, end_c - start_c, )
+    # db.session.flush()
+
+    # start_d = time()
+    # iactive = Campaign.query.join(user_campaign).join(User)
+    # bactive = Campaign.query.join(brand_campaign).join(User)
+    # active_ll_d = iactive.union(bactive).filter(Campaign.completed is False)
+    # result_d = active_ll_d.all()
+    # end_d = time()
+    # results['d'] = (active_ll_d, result_d, end_d - start_d, )
+    # db.session.flush()
+
+    # start_e = time()
+    # initial_e = Campaign.query.filter(Campaign.completed is False)
+    # iactive = initial_e.join(user_campaign).join(User)
+    # bactive = initial_e.join(brand_campaign).join(User)
+    # active_ll_e = iactive.union(bactive)
+    # result_e = active_ll_e.all()
+    # end_e = time()
+    # results['e'] = (active_ll_e, result_e, end_e - start_e, )
+    # db.session.flush()
+    elif version == 'x':
+        start_x = time()
+        got_active = Campaign.completed.is_(False)
+        users_x = User.query.filter(or_(User.campaigns.any(got_active), User.brand_campaigns.any(got_active)))
+        result_x = users_x.all()
+        end_x = time()
+        result = (users_x, result_x, len(result_x), end_x - start_x, )
+    elif version == 'y':
+        start_y = time()
+        has_active = Campaign.completed.is_(False)
+        users_y = User.query.filter(User.instagram_id.isnot(None))
+        users_y = users_y.filter(or_(User.campaigns.any(has_active), User.brand_campaigns.any(has_active)))
+        result_y = users_y.all()
+        end_y = time()
+        results = (users_y, result_y, len(result_y), end_y - start_y, )
+    elif version == 'z':
+        start_z = time()
+        is_active = Campaign.completed.is_(False)
+        users_z = User.query.filter(User.instagram_id.isnot(None), (or_(User.campaigns.any(is_active), User.brand_campaigns.any(is_active))))
+        result_z = users_z.all()
+        end_z = time()
+        results = (users_z, result_z, len(result_z), end_z - start_z, )
+
+    db.session.flush()
+    return results
 
 
 def timeit(func, *args, **kwargs):
