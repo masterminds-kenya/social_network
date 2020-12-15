50d05aa5906320d10a750c449d91dd720889fc57
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-12-14 20:15:09 -0800

    Added a view of all media Posts with error sentinel caption values.

--- application/routes.py
+++ application/routes.py
@@ -127,6 +127,19 @@ def permission_check(mod, id):
     return render_template('view.html', mod=mod, data=data)
 
 
+@app.route('/data/post/problem')
+@admin_required()
+def problem_posts():
+    """These media posts experienced problems that should be investigated. """
+    # app.logger.info("========== Test Method for admin:  ==========")
+    models = Post.query.filter(Post.caption.in_(caption_errors))
+    data = {'posts': models.all()}
+    mod = 'error media posts'
+    template = 'view.html'
+
+    return render_template(template, mod=mod, data=data, caption_errors=caption_errors)
+
+
 @app.route('/data/test')
 @admin_required()
 def test_method():
