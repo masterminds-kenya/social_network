81019a2f90e953e5b47ee78ebf01da1e4c3d130c
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-12-18 03:16:41 -0800

    Typo.

--- application/developer_admin.py
+++ application/developer_admin.py
@@ -100,7 +100,7 @@ def make_permission_overview(data):
             keys = [(f'Permission for {FB_CLIENT_APP_NAME}', 'Platform'), ('Permissions Needed', 'Need Scope')]
             label_vals = []
             for key, label in keys:
-                val = info.get(key, '')
+                val = info.get(key, None)
                 if isinstance(val, (list, tuple)):
                     val = ', '.join(val)
                 elif val is None:
