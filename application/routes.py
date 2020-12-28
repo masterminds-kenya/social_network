0470c5157dd7584bbc27dd9f8132e032e09b2841
Chris L Chapman <ChristopherLChapman42@gmail.com>
2020-12-27 17:24:26 -0800

    Update Admin test route. Using redirect.

--- application/routes.py
+++ application/routes.py
@@ -176,13 +176,15 @@ def problem_posts():
 @admin_required()
 def test_method():
     """Temporary restricted to admin route and function for developer to test components. """
+    from .create_queue_task import list_queues
     app.logger.info("========== Test Method for admin:  ==========")
-    info = get_daily_ig_accounts()
-    pprint([f"{ea}: {len(ea.campaigns)} | {len(ea.brand_campaigns)} " for ea in info])
+    info = list_queues()
+    # info = get_daily_ig_accounts()
+    # pprint([f"{ea}: {len(ea.campaigns)} | {len(ea.brand_campaigns)} " for ea in info])
     # info = {'key1': 1, 'key2': 'two', 'key3': '3rd', 'meaningful': False}
     # pprint(info)
     app.logger.info('-------------------------------------------------------------')
-    return admin(data=info)
+    return redirect(url_for('admin', data=info))
 
 
 @app.route('/any/test')
