63d026350d5a1c97e79ac39559f981fb4766e7f6
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-03-05 14:10:35 -0800

    If temporary time added in media_posts_save, remove from modified media_results.

--- application/manage.py
+++ application/manage.py
@@ -1,5 +1,6 @@
 from flask import flash, redirect, render_template, url_for, request, current_app as app
 from flask_login import current_user, login_user
+from sqlalchemy.sql.sqltypes import DateTime
 from werkzeug.security import generate_password_hash
 from collections import defaultdict
 from functools import reduce
@@ -272,6 +273,10 @@ def media_posts_save(media_results, bulk_db_operation='create', return_ids=False
         app.logger.error(error_info)
         app.logger.error(e)
         db.session.rollback()
+    if add_time and bulk_db_operation == 'create':
+        datefield = 'recorded' if args[0] == Post else 'end_time'
+        for data in mediaset:
+            data.pop(datefield)
     return count, success
 
 
