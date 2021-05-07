e2805d02738a6f3a7f44be07a9edf7707698f621
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-05-07 15:52:28 -0700

    The 'all_posts' route always logs result to 'alert' logger.

--- application/routes.py
+++ application/routes.py
@@ -480,10 +480,10 @@ def all_posts():
         message += "Admin requested getting posts for all active users. "
         flash(message)
         response = redirect(url_for('admin', data=response))
-    if not success:
-        app.alert.critical(message)
+    if success:
+        app.alert.info(message)
     else:
-        app.logger.info(message)
+        app.alert.critical(message)
     return response
 
 
