ec363a411cee30a48e559d01422cc79aaf800e67
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-05-12 19:08:16 -0700

    Set log_client to 'logging' for GAE-standard environment. Update CloudLog name for creating client.

--- application/cloud_log.py
+++ application/cloud_log.py
@@ -25,7 +25,6 @@ class CloudLog(logging.getLoggerClass()):
         self.setLevel(level)
         handler = self.make_handler(handler_name, log_client)
         self.addHandler(handler)
-
         if parent == name:
             parent = None
         elif parent == 'root':
@@ -73,7 +72,7 @@ class CloudLog(logging.getLoggerClass()):
         return name.lower()
 
     @classmethod
-    def make_cloud_log_client(cls, credential_path=None, credentials=None):
+    def make_client(cls, credential_path=None, credentials=None):
         """Creates the appropriate client to be used by other methods. """
         if credential_path:
             credentials = service_account.Credentials.from_service_account_file(credential_path)
@@ -88,7 +87,7 @@ class CloudLog(logging.getLoggerClass()):
         handler_name = cls.make_handler_name(handler_name)
         if cloud:
             if not isinstance(log_client, cloud_logging.Client):
-                log_client = cls.make_cloud_log_client()
+                log_client = cls.make_client()
             handler = CloudLoggingHandler(log_client, name=handler_name)
         else:
             handler = logging.StreamHandler()
@@ -196,7 +195,7 @@ class CloudLog(logging.getLoggerClass()):
 
 def setup_cloud_logging(service_account_path, base_log_level, cloud_log_level, extra=None):
     """Function to setup logging with google.cloud.logging when not on Google Cloud App Standard. """
-    log_client = CloudLog.make_cloud_log_client(service_account_path)
+    log_client = CloudLog.make_client(service_account_path)
     log_client.setup_logging(log_level=base_log_level)  # log_level sets the logger, not the handler.
     # Note: any modifications to the default 'python' handler from setup_logging will invalidate creds.
     handler = CloudLog.make_handler(CloudLog.APP_HANDLER_NAME, log_client, cloud_log_level)
@@ -205,5 +204,5 @@ def setup_cloud_logging(service_account_path, base_log_level, cloud_log_level, e
         extra = []
     elif isinstance(extra, str):
         extra = [extra]
-    cloud_logs = [CloudLog(ea, ea, base_log_level, log_client) for ea in extra]
+    cloud_logs = [CloudLog(name, name, base_log_level, log_client) for name in extra]
     return (log_client, *cloud_logs)
