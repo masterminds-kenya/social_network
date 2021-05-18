09e434b1190973b4efe45c637d176ad93fd8a449
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-05-17 23:53:41 -0700

    The setup_cloud_logging function uses the get_default_handler Client method.

--- application/cloud_log.py
+++ application/cloud_log.py
@@ -226,6 +226,7 @@ class CloudLog(logging.getLoggerClass()):
 def setup_cloud_logging(service_account_path, base_log_level, cloud_log_level, extra=None):
     """Function to setup logging with google.cloud.logging when not on Google Cloud App Standard. """
     log_client = CloudLog.make_client(service_account_path)
+    log_client.get_default_handler()
     log_client.setup_logging(log_level=base_log_level)  # log_level sets the logger, not the handler.
     # Note: any modifications to the default 'python' handler from setup_logging will invalidate creds.
     handler = CloudLog.make_handler(CloudLog.APP_HANDLER_NAME, log_client, cloud_log_level)
