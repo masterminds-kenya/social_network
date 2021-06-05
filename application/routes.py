44b82c1ac3145f88ae42636b718081ec16bece13
Chris L Chapman <ChristopherLChapman42@gmail.com>
2021-06-04 23:29:23 -0700

    Admin test route with code version clarification and visual clarification.

--- application/routes.py
+++ application/routes.py
@@ -185,7 +185,11 @@ def test_method():
     # pprint([f"{ea}: {len(ea.campaigns)} | {len(ea.brand_campaigns)} " for ea in info])
     info = {'key1': 1, 'key2': 'two', 'key3': '3rd', 'meaningful': False, 'testing': 'logging'}
     pprint(info)
-    CloudLog.test_loggers(app, app.log_list, context='s_log & c_log')
+    print("****************************************************************************************")
+    print(app.config.get('GAE_VERSION', 'UNKNOWN VERSION'))
+    print("****************************************************************************************")
+    # pprint(app.config)
+    CloudLog.test_loggers(app, app.log_list, context='no-client')
     print("--------------------------------------------------")
 
     return redirect(url_for('admin', data=info))
